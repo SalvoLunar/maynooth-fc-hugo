27961a407675d2f501990e59f2a7ec1a0713a1b6
Salvatore Vaccarino <salvo@lunarct.com>
2019-06-30 20:15:19 +0100

    local changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -27,8 +27,6 @@ if (process.env.DEBUG) {
 
 gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
-gulp.task("build", ["css", "js", "hugo"]);
-gulp.task("build-preview", ["css", "js", "hugo-preview"]);
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
@@ -71,17 +69,17 @@ gulp.task("svg", () => {
     .pipe(gulp.dest("site/layouts/partials/"));
 });
 
-gulp.task("server", ["hugo", "css", "js", "svg"], () => {
+gulp.task("server", gulp.series("hugo", "css", "js", "svg", () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
     }
   });
-  gulp.watch("./src/js/**/*.js", ["js"]);
-  gulp.watch("./src/css/**/*.css", ["css"]);
-  gulp.watch("./site/static/img/icons-*.svg", ["svg"]);
-  gulp.watch("./site/**/*", ["hugo"]);
-});
+  gulp.watch("./src/js/**/*.js", gulp.series("js"));
+  gulp.watch("./src/css/**/*.css",  gulp.series("css"));
+  gulp.watch("./site/static/img/icons-*.svg",  gulp.series("svg"));
+  gulp.watch("./site/**/*",  gulp.series("hugo"));
+}));
 
 function buildSite(cb, options) {
   const args = options ? defaultArgs.concat(options) : defaultArgs;
@@ -96,3 +94,8 @@ function buildSite(cb, options) {
     }
   });
 }
+
+gulp.task("build", gulp.parallel("css", "js", "hugo"));
+gulp.task("build-preview", gulp.parallel("css", "js", "hugo-preview"));
+
+
