7232fe4cc7f3a5eb28fca70d4236fcdf39335054
Salvatore Vaccarino <salvo@lunarct.com>
2019-08-17 20:16:42 +0100

    function

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,13 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function menuToggle() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
