a16a69a752efa6b4aa0cb64e22b373625c0ca019
Salvatore Vaccarino <salvo@lunarct.com>
2019-08-18 12:23:02 +0100

    menu fixes

--- src/js/app.js
+++ src/js/app.js
@@ -10,8 +10,8 @@ if (window.netlifyIdentity) {
 }
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function menuToggle() {
-  var x = document.getElementById("myTopnav");
+window.menuToggle = function(){
+  var x = document.getElementById("topnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
