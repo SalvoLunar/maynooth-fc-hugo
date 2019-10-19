63c70c1526b2717d3587c281a490fa3dc44e6d1c
Salvatore Vaccarino <salvo@lunarct.com>
2019-10-19 20:55:34 +0100

    changes to homepage

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -10,13 +10,15 @@ export default class PostPreview extends React.Component {
     const entryValues = entry.getIn(["data", "images"]);
     const images = entryValues ? entryValues.toJS() : [];
 
+    console.log(images)
+
     return <div>
-        <Jumbotron image={images[0]} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
+        <Jumbotron image={images[0].imageUrl} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
 
         <div className="bg-grey-1 pv4">
-          <div className="flex-l mhn1-l ph3 center mw7">
-            <h2 className="f2 b lh-title mb2 w-40-l text-center center">{entry.getIn(["data", "welcome"])}</h2>
-            <div className="w-60-l text-center center">{ widgetFor("blurb") }</div>
+          <div className="mhn1-l ph3 center mw7">
+            <h2 className="f2 b lh-title mb2 text-center center">{entry.getIn(["data", "welcome"])}</h2>
+            <div className="text-center center">{ widgetFor("blurb") }</div>
           </div>
         </div>
     </div>
